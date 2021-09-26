a8bc0d95df302703e04bf3f546b108322f5a87a9
provanpm <provanpm@mail.uc.edu>
2021-09-26 18:46:33 -0400

    Initial Commit of Spring Boot Project

---
+++ src/main/java/com/provanpm/provanpm_Assignment0/ProvanpmAssignment0Application.java
@@ -0,0 +1,13 @@
+package com.provanpm.provanpm_Assignment0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvanpmAssignment0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvanpmAssignment0Application.class, args);
+	}
+
+}
